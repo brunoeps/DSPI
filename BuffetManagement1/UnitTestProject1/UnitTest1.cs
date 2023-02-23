84a4400c95033c5c3ea2d58e08aa0ffa4ea35e14
LAB201 <LAB201@serveduc.com>
2023-02-23 19:25:09 -0300

    exercício teste unitário

---
+++ BuffetManagement1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cliente = new BuffetManagement.Modelo.Cliente();
+            cliente.Nome = "Bruno";
+            cliente.Telefone = "190";
+            cliente.Cpf = "12345678910";
+            var cadastro = new BuffetManagement.Negócio.Cliente().Create(cliente);
+            Assert.AreEqual(true, cadastro);
+        }
+    }
+}
