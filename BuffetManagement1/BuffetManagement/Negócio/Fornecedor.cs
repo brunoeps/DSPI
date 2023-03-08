b17d724b543f512318db127cb26e8609a374f58c
sabrinasenai <0000882561@senaimgaluno.com.br>
2023-03-08 19:36:48 -0300

    teste PC Sabrina

---
+++ BuffetManagement1/BuffetManagement/Negócio/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuffetManagement.Negócio
+{
+    public class Fornecedor
+    {
+        private MySqlConnection connection;
+
+        public Fornecedor()
+        {
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+    }
+}
